6029d571ea3903054564213488f71fc1e09deab7
EL-SICRIO-R3 <w.leyvapeiro@gmail.com>
2023-08-27 23:56:03 -0700

    2301-Actualizacion(25)

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[]=[
+    {id: 1, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 2, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 3, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 4, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 5, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 6, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 7, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 8, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+    {id: 9, nombre: 'Will', apellido: 'Leyva', email: 'wwiill@gmail.com', createAt: '2023-07-25'},
+];
